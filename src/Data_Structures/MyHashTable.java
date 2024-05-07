3274f8b871a17552c7ea9dd62e6b39f0afff7b6a
suncak3 <tignat69228@gmail.com>
2024-05-08 00:40:55 +0500

    I have created the MyHashTable class with the implementation of necessary methods and some documentation for them.

--- src/Data_Structures/MyHashTable.java
+++ src/Data_Structures/MyHashTable.java
@@ -22,4 +22,145 @@ public class MyHashTable<K, V> {
     private int M = 11;
     private int size;
 
+
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    /**
+     * Constructor that initialise hash node with specific number of buckets.
+     * @param M
+     */
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    /**
+     * Function that returns the index for key in the chainArray.
+     * @param key key to give the index for.
+     * @return index in the chainArray.
+     */
+    private int hash(K key) {
+         return Math.abs(key.hashCode()%M);
+    }
+
+    /**
+     * Adds the new key-value pair int the chainArray.
+     * @param key key to be added
+     * @param value value to be added
+     */
+    public void put(K key, V value) {
+         int index = hash(key);
+         HashNode<K, V> h = chainArray[index];
+
+         while(h != null){
+             if(h.key.equals(key)){
+                 h.value = value;
+                 return;
+             }
+             h = h.next;
+         }
+
+         size++;
+         HashNode<K, V> newNode = new HashNode<>(key, value);
+         newNode.next = chainArray[index];
+         chainArray[index] = newNode;
+
+         //Resizing when load factor more than 0.7
+         if((1.0 * size) / M > 0.7){
+             M *= 2;
+             HashNode<K, V>[] newChainArray = new HashNode[M];
+
+             for (int i = 0; i < chainArray.length; i++) {
+                 HashNode<K, V> head = chainArray[i];
+                 while(head != null){
+                     int newIndex = hash(head.key);
+                     HashNode<K, V> next = head.next;
+                     head.next = newChainArray[newIndex];
+                     newChainArray[newIndex] = head;
+                     head = next;
+                 }
+             }
+             chainArray = newChainArray;
+         }
+    }
+
+    /**
+     * Returns the value of the given key.
+     * @param key key of which value to be fined.
+     * @return the value of key, or null of there is no such key.
+     */
+    public V get (K key){
+         int index = hash(key);
+         HashNode<K, V> h = chainArray[index];
+
+         while(h != null){
+             if(h.key.equals(key)){
+                 return h.value;
+             }
+             h = h.next;
+         }
+         return null;
+    }
+
+    /**
+     * Removes and returns the value of given key.
+     * @param key key of which value is to be removed.
+     * @return the removed value.
+     */
+    public V remove(K key){
+         int index = hash(key);
+         HashNode<K, V> h = chainArray[index];
+         HashNode<K, V> prev = null;
+
+         while(h != null){
+             if(h.key.equals(key)){
+                 break;
+             }
+             prev = h;
+             h = h.next;
+         }
+         if(h == null){
+             return null;
+         }
+         if(prev != null){
+            prev.next = h.next;
+         }
+         else{
+             chainArray[index] = h.next;
+         }
+         size--;
+         return h.value;
+    }
+
+    /**
+     * Determines the existence of the given value.
+     * @param value value to be checked.
+     * @return true if exists, or false otherwise.
+     */
+    public boolean contains(V value){
+        K res = getKey(value);
+        return res != null;
+    }
+
+    /**
+     * Returns the key of given value.
+     * @param value value of which key is to be fined.
+     * @return the key of a given value.
+     */
+    public K getKey(V value){
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> h = chainArray[i];
+            while(h != null){
+                if(h.value == null ? value == null : h.value.equals(value)){
+                    return h.key;
+                }
+                h = h.next;
+            }
+        }
+        return null;
+    }
 }
---
+++ src/Testers/MyTestingClass.java
@@ -0,0 +1,4 @@
+package Testers;
+
+public class MyTestingClass {
+}
