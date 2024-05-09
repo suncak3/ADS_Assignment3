7888888b4d83e697203dfc71598b747aced11a05
suncak3 <tignat69228@gmail.com>
2024-05-10 00:40:47 +0500

    Final commit, some additions in the BST class according to the last part of assignment. Creation of a user based interface in the main class for more comfortability.

--- src/Data_Structures/BST.java
+++ src/Data_Structures/BST.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class BST <K extends Comparable<K>, V>{
     private Node root;
+    private int size;
 
+    /**
+     * Represents a node of the binary tree.
+     */
     private class Node{
         private K key;
         private V value;
@@ -16,12 +20,25 @@ public class BST <K extends Comparable<K>, V>{
         }
     }
 
+    /**
+     * Adds or updates a value on the given key.
+     * @param key key to insert or update
+     * @param value value to be inserted
+     */
     public void put(K key, V value){
         root = put(root, key, value);
     }
 
+    /**
+     * Helper method to put the value in the recursive form.
+     * @param h the node
+     * @param key key to insert or update
+     * @param value value to be inserted
+     * @return updated node after insertion or update
+     */
     private Node put(Node h, K key, V value){
         if(h == null){
+            size += 1;
             return new Node(key, value);
         }
         int temp = key.compareTo(h.key);
@@ -37,6 +54,11 @@ public class BST <K extends Comparable<K>, V>{
         return h;
     }
 
+    /**
+     * Gives the value of specific key.
+     * @param key key which value is to be given.
+     * @return value of the key or null
+     */
     public V get(K key){
         Node h = get(root, key);
         if(h == null){
@@ -45,6 +67,12 @@ public class BST <K extends Comparable<K>, V>{
         return h.value;
     }
 
+    /**
+     * Helper method tp get the value of the given key.
+     * @param h the node
+     * @param key key which value is to be given.
+     * @return Node with specific key
+     */
     private Node get(Node h, K key){
         if(h == null){
             return null;
@@ -61,10 +89,20 @@ public class BST <K extends Comparable<K>, V>{
         }
     }
 
+    /**
+     * Deletes a pair key-value.
+     * @param key key to be deleted
+     */
     public void delete(K key){
         root = delete(root, key);
     }
 
+    /**
+     * Helper method to delete the key-value pair.
+     * @param h the node
+     * @param key key to be deleted
+     * @return updated node after deletion
+     */
     private Node delete(Node h, K key){
         if(h == null){
             return null;
@@ -78,9 +116,11 @@ public class BST <K extends Comparable<K>, V>{
         }
         else{
             if(h.left == null){
+                size -= 1;
                 return h.right;
             }
             if(h.right == null){
+                size -= 1;
                 return h.left;
             }
             h.key = minValue(h.right);
@@ -89,6 +129,11 @@ public class BST <K extends Comparable<K>, V>{
         return h;
     }
 
+    /**
+     * Finds the minimum key in the right subtree.
+     * @param h root of subtree
+     * @return minimum key
+     */
     private K minValue(Node h){
         K minValue = h.key;
         while(h.left != null){
@@ -98,18 +143,58 @@ public class BST <K extends Comparable<K>, V>{
         return minValue;
     }
 
-    public Iterable<K> iterator(){
-        List<K> keys = new ArrayList<>();
-        inOrderTraversal(root, keys);
-        return keys;
+    /**
+     * Returns number of elements
+     * @return the size of BST
+     */
+    public int size(){
+        return size;
     }
 
-    private void inOrderTraversal(Node h, List<K> keys){
+    /**
+     * An additional class to provide key-value pair access.
+     * @param <K>
+     * @param <V>
+     */
+    public static class Pair<K, V>{
+        private final K key;
+        private final V value;
+
+        public Pair(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * Provides an iterator over the key-value pairs
+     * @return collection of pairs
+     */
+    public Iterable<Pair<K, V>> iterator(){
+        List<Pair<K, V>> elements = new ArrayList<>();
+        inOrderTraversal(root, elements);
+        return elements;
+    }
+
+    /**
+     * helper method to make in-order traversal of the binary tree.
+     * @param h the node
+     * @param elements collection of data
+     */
+    private void inOrderTraversal(Node h, List<Pair<K, V>> elements){
         if(h == null){
             return;
         }
-        inOrderTraversal(h.left, keys);
-        keys.add(h.key);
-        inOrderTraversal(h.right, keys);
+        inOrderTraversal(h.left, elements);
+        elements.add(new Pair<>(h.key, h.value));
+        inOrderTraversal(h.right, elements);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,37 @@
+import Data_Structures.BST;
 import Data_Structures.MyHashTable;
 import Testers.MyTestingClass;
 
 import java.util.Random;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select the part that you want to test:");
+        System.out.println("1. Part 1.2 (Hash Table)");
+        System.out.println("2. Part 2.2 (BST)");
+        System.out.print("Enter your choice (1 or 2): ");
+        int n = scanner.nextInt();
+
+        switch (n) {
+            case 1:
+                testHashTable();
+                break;
+            case 2:
+                testBST();
+                break;
+            default:
+                System.out.println("Invalid choice. Please select either 1 or 2.");
+                break;
+        }
+    }
+
+    private static void testHashTable() {
         MyHashTable<MyTestingClass, User> hashTable = new MyHashTable<>();
         Random random = new Random();
 
-        for (int i = 1; i <= 10000 ; i++) {
+        for (int i = 1; i <= 10000; i++) {
             int id = random.nextInt(10000);
             String name = "User" + i;
             MyTestingClass key = new MyTestingClass(id, name);
@@ -18,5 +39,17 @@ public class Main {
             hashTable.put(key, user);
         }
         hashTable.printSizeOfBuckets();
+    }
+
+    private static void testBST() {
+        BST<Integer, String> bst = new BST<>();
+        bst.put(1, "Nursultan Khaimuldin");
+        bst.put(3, "Askar Khaimuldin");
+        bst.put(4, "Balembay Balembayev");
+        bst.put(2, "Almata is the best city");
+
+        for (BST.Pair<Integer, String> elem : bst.iterator()) {
+            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+        }
     }
 }
